dafb9d51a0492371e651ae3e322071d356e45bdf
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-21 22:15:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,3 +21,4 @@ function Sandwich (bread, ingredients, cut) {
 var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
 var reuben = new Sandwich("rye", ["corned beef","sauerkraut","swiss","russian dressing"],"diagonal");
 
+pbj.serve();
