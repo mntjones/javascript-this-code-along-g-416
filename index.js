407cacdcbd8984e3642517bfcdd419e7d9177968
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-21 22:13:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much."
+
+var pbj = {
+  bread: 'white',
+  ingredients: ['peanut butter', 'jelly'],
+  cut: 'triangles'
+};
+
+
+//constructor
+function Sandwich (bread, ingredients, cut) {
+  this.bread = bread;
+  this.ingredients = ingredients;
+  this.cut = cut;
+}
+
+var blt = new Sandwich('white')
