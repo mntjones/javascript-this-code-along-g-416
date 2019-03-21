f805386d7bdb63a206e31fd1771377735f6a1e56
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-21 22:14:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const app = "I don't do much."
 var pbj = {
   bread: 'white',
   ingredients: ['peanut butter', 'jelly'],
-  cut: 'triangles'
+  cut: 'triangles',
+  name: "peanut butter and jelly",
+  serve: function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
 };
 
 
