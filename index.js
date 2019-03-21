e46d82fa9607f62d0b499d1576e11516640709e1
Monica Tamaru Jones <mntamaru@gmail.com>
2019-03-21 22:22:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ var pbj = {
   ingredients: ['peanut butter', 'jelly'],
   cut: 'triangles',
   name: "peanut butter and jelly",
-  serve: function() {
-    console.log("here's your " + this.name + ", enjoy!");
-  }
+  
 };
 
 
@@ -15,7 +13,9 @@ var pbj = {
 function Sandwich (bread, ingredients, cut) {
   this.bread = bread;
   this.ingredients = ingredients;
-  this.cut = cut;
+  this.cut = cut;serve: function() {
+    console.log("here's your " + this.name + ", enjoy!");
+  }
 }
 
 var blt = new Sandwich("white", ["bacon","lettuce","tomato","mayo"],"rectangle");
